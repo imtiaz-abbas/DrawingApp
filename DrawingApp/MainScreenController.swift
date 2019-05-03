1622d7ce5c907716ec32f8953b2cc62a551c9ddd
imtiaz.a <imtiaz@uncommon.is>
2019-05-03 19:34:08 +0530

    structured views

--- DrawingApp/MainScreenController.swift
+++ DrawingApp/MainScreenController.swift
@@ -17,29 +17,54 @@ class MainScreenController: UIViewController {
     let redoButton = UIButton(type: UIButton.ButtonType.system)
     let resetButton = UIButton(type: UIButton.ButtonType.system)
     var mainImageView = CanvasView()
+    
+    var toolBar = UIView()
+    var bottomBar = UIView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         let screenSize = UIScreen.main.bounds
         view.frame = CGRect(origin: .zero, size: CGSize(width: screenSize.width, height: screenSize.height))
-        view.backgroundColor = .red
-        mainImageView.frame = CGRect(origin: .zero, size: CGSize(width: screenSize.width, height: screenSize.height))
-//        view.addSubview(mainImageView)
-        view.sv(mainImageView)
+        view.backgroundColor = .white
+        
+        //adding subviews to mainContainer
+        view.sv(toolBar, mainImageView, bottomBar)
+        
+        toolBar.height(100)
+        toolBar.width(screenSize.width)
+        toolBar.Top == .zero
+        
+        bottomBar.height(100)
+        bottomBar.width(screenSize.width)
+        bottomBar.Bottom == .zero
+        
+        
+        mainImageView.Top == toolBar.Bottom
+        mainImageView.Bottom == bottomBar.Top
+        mainImageView.height(screenSize.height - 200)
+        mainImageView.width(screenSize.width)
+        
         mainImageView.backgroundColor = .white
+//        mainImageView.frame = CGRect(origin: .zero, size: CGSize(width: screenSize.width, height: screenSize.height))
+//        mainImageView.backgroundColor = .white
         mainImageView.setup()
-        view.sv(undoButton)
-        view.sv(redoButton)
-        view.sv(resetButton)
+        
+        //adding subviews to bottomBar
+        bottomBar.sv(undoButton, redoButton, resetButton)
+        
+        undoButton.Left == 20
         undoButton.Bottom == 50
+        undoButton.text("Undo")
+        
         redoButton.Bottom == 50
+        redoButton.centerHorizontally()
+        redoButton.text("Redo")
+        
         resetButton.Bottom == 50
         resetButton.Right == 20
-        undoButton.Left == 20
         resetButton.text("Reset")
-        undoButton.text("Undo")
-        redoButton.text("Redo")
-        redoButton.centerHorizontally()
+        
+        // adding actions to all the buttons in bottom bar
         undoButton.addTarget(self, action: #selector(undo), for:.touchUpInside)
         redoButton.addTarget(self, action: #selector(redo), for:.touchUpInside)
         resetButton.addTarget(self, action: #selector(reset), for:.touchUpInside)
